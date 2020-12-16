61b242b4439897ba51d73f0239a17f5af581a823
Primadika Dwijantara <primadika.dwijantara@gmail.com>
2020-12-16 15:03:29 +0700

    Updates

---
+++ precache-manifest.c11223e849d90baf299ddc9b9887aff8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f45d723ddc5800f60da",
+    "url": "/analog-clock-js/static/css/main.052fe8f3.chunk.css"
+  },
+  {
+    "revision": "4f45d723ddc5800f60da",
+    "url": "/analog-clock-js/static/js/main.de029fa9.chunk.js"
+  },
+  {
+    "revision": "23ef285e2879f2cf25a0",
+    "url": "/analog-clock-js/static/js/runtime~main.fbf48b4a.js"
+  },
+  {
+    "revision": "a0e51b5d9ffcfd5ef198",
+    "url": "/analog-clock-js/static/js/2.057ce707.chunk.js"
+  },
+  {
+    "revision": "d5746c991d9f378fe37d22d7f2ce2762",
+    "url": "/analog-clock-js/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/analog-clock-js/precache-manifest.c11223e849d90baf299ddc9b9887aff8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/analog-clock-js/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
